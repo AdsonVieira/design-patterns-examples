00b7f8edb4526cf7039bf8de54148843cda2a96a
AdsonVieira <adsonvieira@live.com>
2023-04-15 19:03:07 -0300

    Fix space

--- src/creational/singleton/Logger.java
+++ src/creational/singleton/Logger.java
@@ -4,6 +4,7 @@ public class Logger {
     private static Logger instance;
 
     private Logger() {}
+
     public static Logger getInstance() {
         if (instance == null) {
             return new Logger();
