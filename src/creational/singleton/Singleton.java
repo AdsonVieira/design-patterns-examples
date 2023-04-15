cfd634b723abd5459971b722aafca228e3f73c90
AdsonVieira <adsonvieira@live.com>
2023-04-15 19:01:43 -0300

    Singleton

---
+++ src/creational/singleton/Singleton.java
@@ -0,0 +1,8 @@
+package creational.singleton;
+
+public class Singleton {
+    public static void main(String[] args) {
+        Logger logger = Logger.getInstance();
+        logger.getInstanceInfo();
+    }
+}
