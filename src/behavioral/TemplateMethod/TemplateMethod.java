0d8f448d62a34ec26eb3856c62b0e538e384e271
AdsonVieira <adsonvieira@live.com>
2023-04-17 22:43:23 -0300

    Add TemplateMethod

---
+++ src/behavioral/TemplateMethod/TemplateMethod.java
@@ -0,0 +1,8 @@
+package behavioral.TemplateMethod;
+
+public class TemplateMethod {
+    public static void main(String[] args) {
+        EmployeeCLT employeeCLT = new EmployeeCLT();
+        System.out.println(employeeCLT.calcSalary());
+    }
+}
